f998d75462d23ee83bbc3dcad43f9b50c8b4f137
FerlinGH <Grygorenko.d@ukr.net>
2017-04-10 19:07:25 +0300

    Delete Human.java

--- Lesson02Home2/src/net/ukr/grygorenko_d/Human.java
+++
@@ -1,9 +0,0 @@
-package net.ukr.grygorenko_d;
-
-public class Human {
-    private String name;
-    private String surname;
-    private String[] phones;
-    private String[] sites;
-    private Address address;
-}
